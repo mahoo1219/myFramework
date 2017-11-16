62511d85c82851849c2574037267987376d6bd84
mahoo1219 <mahoo1219@gmail.com>
2017-11-16 16:09:00 +0800

    third

--- src/main/java/org/mahoo/framework/HelperLoader.java
+++ src/main/java/org/mahoo/framework/HelperLoader.java
@@ -1,9 +1,6 @@
 package org.mahoo.framework;
 
-import org.mahoo.framework.helper.BeanHelper;
-import org.mahoo.framework.helper.ClassHelper;
-import org.mahoo.framework.helper.ControllerHelper;
-import org.mahoo.framework.helper.IocHelper;
+import org.mahoo.framework.helper.*;
 import org.mahoo.framework.util.ClassUtil;
 
 /**
@@ -14,11 +11,12 @@ public final class HelperLoader {
         Class<?>[] classList = {
                 ClassHelper.class,
                 BeanHelper.class,
+                AopHelper.class,
                 IocHelper.class,
                 ControllerHelper.class
         };
         for (Class<?> cls : classList) {
-            ClassUtil.loadClass(cls.getName());
+            ClassUtil.loadClass(cls.getName(), true);
         }
     }
 }
--- src/main/java/org/mahoo/framework/helper/BeanHelper.java
+++ src/main/java/org/mahoo/framework/helper/BeanHelper.java
@@ -39,4 +39,11 @@ public final class BeanHelper {
         }
         return (T) BEAN_MAP.get(cls);
     }
+
+    /**
+     * 设置Bean 实例
+     */
+    public static void setBean(Class<?> cls, Object object) {
+        BEAN_MAP.put(cls, object);
+    }
 }
